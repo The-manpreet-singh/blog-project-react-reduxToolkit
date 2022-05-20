0278cfe82b1590fe218e594b3ecb41d67757ca90
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-05-20 11:21:55 +0530

    finish counter

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,22 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    count:0
-}
+  count: 0,
+};
 
-export const counterSlice= createSlice({
-    name:'counter',
-    initialState,
-    reducers:{
-        increment: (state)=> {
-            state.count += 1;
-        },
-        decrement: (state)=> {
-            state.count -= 1;
-        }
-    }
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    //add all actions here
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+  },
 });
 
-export const {increment, decrement}= counterSlice.actions; //destructures both actions from counterSlice
+export const { increment, decrement, reset, incrementByAmount } =
+  counterSlice.actions; //destructures both actions from counterSlice
 
 export default counterSlice.reducer; //full reducer export
